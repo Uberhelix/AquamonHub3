33ae32d601b9c43b64bee4e1c97157b3acbc5073
Stephen Hill <citizenhill@gmail.com>
2017-01-12 15:06:45 -0330

    Initial commit.
    Initial commit. Need to work on multiple sensor strings and power
    management.

---
+++ AquamonHub3.ino/AquamonHub3.ino.ino
@@ -0,0 +1,339 @@
+/* RFM69 library and code by Felix Rusu - felix@lowpowerlab.com
+// Get libraries at: https://github.com/LowPowerLab/
+// Make sure you adjust the settings in the configuration section below !!!
+// **********************************************************************************
+// Copyright Felix Rusu, LowPowerLab.com
+// Library and code by Felix Rusu - felix@lowpowerlab.com
+// **********************************************************************************
+// License
+// **********************************************************************************
+// This program is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU General    
+// Public License as published by the Free Software       
+// Foundation; either version 3 of the License, or        
+// (at your option) any later version.                    
+//                                                        
+// This program is distributed in the hope that it will   
+// be useful, but WITHOUT ANY WARRANTY; without even the  
+// implied warranty of MERCHANTABILITY or FITNESS FOR A   
+// PARTICULAR PURPOSE. See the GNU General Public        
+// License for more details.                              
+//                                                        
+// You should have received a copy of the GNU General    
+// Public License along with this program.
+// If not, see <http://www.gnu.org/licenses></http:>.
+//                                                        
+// Licence can be viewed at                               
+// http://www.gnu.org/licenses/gpl-3.0.txt
+//
+// Please maintain this license information along with authorship
+// and copyright notices in any redistribution of this code
+// **********************************************************************************/
+
+#include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <SPI.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <Ethernet.h> 
+#include <EthernetClient.h>
+#include <Dns.h>
+#include <Dhcp.h>
+//----------------------------------------RF set-up
+//*********************************************************************************************
+// *********** IMPORTANT SETTINGS - YOU MUST CHANGE/ONFIGURE TO FIT YOUR HARDWARE *************
+//*********************************************************************************************
+#define NETWORKID     100  //the same on all nodes that talk to each other
+#define NODEID        1
+
+//Match frequency to the hardware version of the radio on your Feather
+#define FREQUENCY     RF69_433MHZ
+//#define FREQUENCY     RF69_868MHZ
+//#define FREQUENCY      RF69_915MHZ
+#define ENCRYPTKEY     "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
+#define IS_RFM69HCW    true // set to 'true' if you are using an RFM69HCW module
+
+//*********************************************************************************************
+#define SERIAL_BAUD   115200
+
+/*for Feather 32u4
+#define RFM69_CS      8
+#define RFM69_IRQ     7
+#define RFM69_IRQN    4  // Pin 7 is IRQ 4!
+#define RFM69_RST     4
+*/
+
+/*for Feather M0 
+#define RFM69_CS      8
+#define RFM69_IRQ     3
+#define RFM69_IRQN    3  // Pin 3 is IRQ 3!
+#define RFM69_RST     4
+*/
+
+/* ESP8266 feather w/wing
+#define RFM69_CS      2
+#define RFM69_IRQ     15
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+#define RFM69_RST     16
+*/
+
+/* Feather 32u4 w/wing
+#define RFM69_CS      10   // "B"
+#define RFM69_RST     11   // "A"
+#define RFM69_IRQ     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+*/
+
+/* Feather m0 w/wing
+#define RFM69_CS      10   // "B"
+#define RFM69_RST     11   // "A"
+#define RFM69_IRQ     6    // "D"
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+*/
+
+/* Teensy 3.x w/wing
+#define RFM69_RST     9   // "A"
+#define RFM69_CS      10   // "B"
+#define RFM69_IRQ     4    // "C"
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+*/
+
+/* WICED Feather w/wing
+#define RFM69_RST     PA4     // "A"
+#define RFM69_CS      PB4     // "B"
+#define RFM69_IRQ     PA15    // "C"
+#define RFM69_IRQN    RFM69_IRQ
+*/
+
+//Breakout
+#define RFM69_CS      8//was 10
+#define RFM69_IRQ     2
+#define RFM69_IRQN    0  // Pin 2 is IRQ 0!
+#define RFM69_RST     9
+
+
+#define LED           13  // onboard blinky
+//#define LED           0 //use 0 on ESP8266
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+RFM69 radio = RFM69(RFM69_CS, RFM69_IRQ, IS_RFM69HCW, RFM69_IRQN);
+String StringPacket; //For parseing packet;
+//---------------------------------------------End of RF set-up
+
+//---------------------------------------------Ethernet set-up
+byte mac[] = {0x74,0x69,0x69,0x2D,0x30,0x31};
+
+//Uncomment the following, and set to a valid ip if you don't have dhcp available.
+//IPAddress iotIP (192, 168, 0, 42);
+//Uncomment the following, and set to your preference if you don't have automatic dns.
+//IPAddress dnsIP (8, 8, 8, 8);
+//If you uncommented either of the above lines, make sure to change "Ethernet.begin(mac)" to "Ethernet.begin(mac, iotIP)" or "Ethernet.begin(mac, iotIP, dnsIP)"
+
+//---------------------------------------------End of Ethernet set-up
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "Uberhelix"
+#define AIO_KEY         "16d05dcf36984185a0bec38356f0e026"
+/*********************** End of Adafruit.io Setup****************************/
+
+/************ Global State (you don't need to change this!) ******************/
+
+//Set up the ethernet client
+EthernetClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+// You don't need to change anything below this line!
+#define halt(s) { Serial.println(F( s )); while(1);  }
+
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/feeds/photocell");
+//Adafruit_MQTT_Publish rom408814430 = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/feeds/rom408814430");
+
+void setup() {
+   Serial.begin(SERIAL_BAUD);
+
+ 
+//-----------------For RF 
+  Serial.println("Feather RFM69HCW Receiver");
+
+  // Hard Reset the RFM module
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, HIGH);
+  delay(100);
+  digitalWrite(RFM69_RST, LOW);
+  delay(100);
+
+  // Initialize radio
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  if (IS_RFM69HCW) {
+    radio.setHighPower();    // Only for RFM69HCW & HW!
+  }
+  radio.setPowerLevel(31); // power output ranges from 0 (5dBm) to 31 (20dBm)
+
+  radio.encrypt(ENCRYPTKEY);
+
+  pinMode(LED, OUTPUT);
+
+  Serial.print("\nListening at ");
+  Serial.print(FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(" MHz");
+
+//----------------For io.adafruit.com
+// Initialise the Client
+  Serial.print(F("\nInit the Client..."));
+  Ethernet.begin(mac);
+  delay(1000); //give the ethernet a second to initialize
+
+ 
+}
+
+
+uint32_t x=0;
+
+
+void loop() {
+  Serial.print("*");
+  //check if something was received (could be an interrupt from the radio)
+  if (radio.receiveDone())
+  {
+    //print message received to serial
+    Serial.print('[');Serial.print(radio.SENDERID);Serial.print("] ");
+    ToIOT((char*)radio.DATA); //Send packet to IOT server function
+    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+  }
+
+  radio.receiveDone(); //put radio in RX mode
+  Serial.flush(); //make sure all serial data is clocked out before sleeping the MCU
+
+/********************* MQTT Demo *****************************/
+
+// Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // Now we can publish stuff!
+  Serial.print(F("\nSending photocell val "));
+  Serial.print(find_value("Data=",StringPacket));
+  Serial.print("...");
+
+  
+  if (! photocell.publish(find_value("Data=",StringPacket))) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+  // ping the server to keep the mqtt connection alive
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+
+/******************** End of MQTT Demo **********************/ 
+}
+
+//****FUNCTIONS****
+
+void Blink(byte PIN, byte DELAY_MS, byte loops)
+{
+  for (byte i=0; i<loops; i++)
+  {
+    digitalWrite(PIN,HIGH);
+    delay(DELAY_MS);
+    digitalWrite(PIN,LOW);
+    delay(DELAY_MS);
+  }
+}
+
+void ToIOT(char *ptrpacket){
+  Serial.println(ptrpacket);
+  StringPacket = ptrpacket;
+
+  //YOU ARE HERE
+  float NetworkID = find_value("NetworkID=",StringPacket);
+  float Node = find_value("NodeId=",StringPacket);
+  String sensorName = find_stringValue("Sensor=",StringPacket);
+  float Data = find_value("Data=",StringPacket); //This needs to be turned into float
+  
+  Serial.print("Data=");
+  Serial.println(Data);
+  
+  /*if (! photocell.publish(Data)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+*/
+  
+}
+
+//For finding value of given string segment.
+float find_value(String needle, String haystack) {
+  int foundpos = -1;
+  int foundposend = -1;
+  float value;
+  for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+    if (haystack.substring(i,needle.length()+i) == needle) {
+      foundpos = i;
+    }
+  }
+  //now find the %
+  for (int i = foundpos; i <= haystack.length() - needle.length(); i++) {
+    if (haystack.substring(i,needle.length()+i) == "%") {
+      int foundposend = i;
+    }
+    String strvalue = haystack.substring(foundpos+needle.length(),foundposend); //The value between the % and the needle.
+    value = strvalue.toFloat(); //Convert to float.
+  }
+  return value;
+}
+
+//For finding string values in radiopacket
+String find_stringValue(String needle, String haystack) {
+  int foundpos = -1;
+  int foundposend = -1;
+  String strvalue;
+  for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+    if (haystack.substring(i,needle.length()+i) == needle) {
+      foundpos = i;
+     }
+  }
+  //now find the %
+  for (int i = foundpos+needle.length(); i <= haystack.length()- needle.length(); i++) {
+    
+    if (haystack.substring(i,i+1) == "%") {
+      int foundposend = i;
+      String strvalue = haystack.substring(foundpos+needle.length(),foundposend); //The value between the % and the needle.
+      return strvalue;
+    }
+  }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+  }
+  Serial.println("MQTT Connected!");
+}
